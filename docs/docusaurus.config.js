c108a67a146ebccdb4f5cb667673148029b90cd4
Moddinu <Moddinu@users.noreply.github.com>
2025-10-23 16:27:18 +0200

    Create docusaurus.config.js

---
+++ docs/docusaurus.config.js
@@ -0,0 +1,40 @@
+// @ts-check
+const config = {
+  title: 'Data Engineering Toolkit',
+  tagline: 'My personal data engineering & analytics knowledge base',
+  favicon: 'img/favicon.ico',
+  url: 'https://Moddinu.github.io',
+  baseUrl: '/data-engineering-toolkit/',
+  organizationName: 'Moddinu',
+  projectName: 'data-engineering-toolkitt',
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false,
+
+  presets: [
+    [
+      'classic',
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl: 'https://github.com/Moddinu/data-engineering-toolkit/edit/main/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig: {
+    navbar: {
+      title: 'Data Engineering Toolkit',
+      items: [{ to: '/docs/intro', label: 'Docs', position: 'left' }],
+    },
+    footer: {
+      style: 'dark',
+      copyright: `Built with ❤️ using Docusaurus — © ${new Date().getFullYear()} YOU`,
+    },
+  },
+};
+
+module.exports = config;
