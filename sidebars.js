3a2789f11987d827dcca57aad852a82fea484a49
Moddinu <Moddinu@users.noreply.github.com>
2025-10-31 08:37:05 +0100

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -25,6 +25,7 @@ module.exports = {
       type: 'category',
       label: 'Data Modeling',
       items: [
+        'modeling/fact-data-modeling'
         'modeling/star-schema',
         'modeling/scd-type2',
       ],
